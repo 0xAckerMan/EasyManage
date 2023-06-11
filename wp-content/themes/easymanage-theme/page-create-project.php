914600d92cd3c19d6854aeea491d52b2eb066501
0xAckerMan <joelkores1@gmail.com>
2023-06-11 22:13:46 +0300

    create project form

--- wp-content/themes/easymanage-theme/page-create-project.php
+++ wp-content/themes/easymanage-theme/page-create-project.php
@@ -1,7 +1,61 @@
 <?php
+
+/**
+ * 
+ * Template Name: Create Project Template
+ */
 get_header();
 ?>
 
-aksdfh;l
+<?php
+global $form_error;
 
-<?php get_footer(); ?>
+$project_id = 1;
+
+$users = [
+    [
+        'id' => 1,
+        'email' => 'johnsmith@localhost.com'
+    ]
+];
+?>
+
+<form action="" method="post">
+    <input type="hidden" name="p_id" value="<?php echo $project_id ?>">
+    <div class="page-create-project">
+
+        <div class="inner-form">
+            <h2>Create Project</h2>
+
+            <p class="error"><?php echo $form_error; ?></p>
+
+            <?php echo do_shortcode("[input_tag name='p_name' label='Project Name' placeholder='Enter the project name']") ?>
+            <?php echo do_shortcode("[input_tag name='p_category' label='Project Category' placeholder='e.g. Mobile App, Web App']") ?>
+            <?php echo do_shortcode("[input_tag name='p_excerpt' label='Project Category' placeholder='e.g. Mobile App, Web App']") ?>
+
+            <div class="input-con">
+                <label for="p_description">Project Description</label>
+                <textarea name="p_description" id="p_description" placeholder="Briefly explain this project"></textarea>
+            </div>
+
+            <?php echo do_shortcode("[input_tag name='p_due_date' input_type='date' label='Project Due Date']") ?>
+
+            <div class="input-con">
+                <label for="p_assigned_to">Assign Project To </label>
+                <select name="p_assigned_to" id="p_assigned_to">
+                    <option value="" selected disabled hidden>Assign to a employee</option>
+                    <?php
+                    foreach ($users as $user) {
+                    ?>
+                        <option value="<?php echo $user['id'] ?>"><?php echo $user['email'] ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <button class="custom-btn" type="submit" name="create-project">Create</button>
+        </div>
+    </div>
+</form>
+<?php get_footer() ?>
