cc2451ddafb4dd18b9ceedd0ae2916b8820073a3
0xAckerMan <joelkores1@gmail.com>
2023-06-12 17:04:06 +0300

    added the copyright footer

---
+++ wp-content/themes/easymanage-theme/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <?php echo bloginfo('name') ?> &copy 2023
+</footer>
+</div>
+
+</div>
+
+</div>
+
+
+
+
+
+
+<script>
+    var date = new Date();
+    new Date().setDate(date.getDate() + 2);
+    var tomorrow = date.toISOString().split('T')[0];
+
+    var dateInputs = document.querySelectorAll('input[type="date"]');
+    dateInputs.forEach(function(input) {
+        input.min = tomorrow;
+    });
+</script>
+
+</body>
+
+</html>
