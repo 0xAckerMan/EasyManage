7b664bbabbe9f47c6af7e12f8612a2a373839db9
0xAckerMan <joelkores1@gmail.com>
2023-06-20 17:28:20 +0300

    updated the projects table

--- wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
+++ wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
@@ -15,26 +15,27 @@ class CreateTables {
 
     function create_table_projects() {
         global $wpdb;
-
+    
         $table_name = $wpdb->prefix . 'projects';
-
+    
         $project_details = "CREATE TABLE IF NOT EXISTS " . $table_name . "(
             p_id INT AUTO_INCREMENT PRIMARY KEY,
             p_name VARCHAR(255) NOT NULL,
             p_description TEXT NOT NULL,
             p_category VARCHAR(255) NOT NULL,
-            p_exerpt VARCHAR(255) NOT NULL,
+            p_excerpt VARCHAR(255) NOT NULL,
             p_status INT DEFAULT 0 NOT NULL,
             p_created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            p_end_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            p_due_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
             p_assigned_to mediumint(9) NOT NULL,
             p_assigned_by mediumint(9) NOT NULL,
             p_cohort_id INT NOT NULL
         )";
-
+    
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($project_details);
     }
+    
 
     function create_table_tasks(){
         global $wpdb;
