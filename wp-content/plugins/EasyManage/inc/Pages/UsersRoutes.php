508e60126b5b3a1af611dc0ca2eb92499107c95f
0xAckerMan <joelkores1@gmail.com>
2023-06-21 13:38:50 +0300

    Up to backend presentation

--- wp-content/plugins/EasyManage/inc/Pages/UsersRoutes.php
+++ wp-content/plugins/EasyManage/inc/Pages/UsersRoutes.php
@@ -46,7 +46,7 @@ class UsersRoutes
             'methods' => 'PUT',
             'callback' => array($this, 'update_user'),
             'permission_callback' => function () {
-                return current_user_can('manage_options');
+                return current_user_can('edit_posts');
             }
         ));
 
@@ -65,75 +65,6 @@ class UsersRoutes
                 return current_user_can('manage_options');
             }
         ));
-
-        // Endpoint for getting trainers
-        register_rest_route('api/v1', 'users/trainers', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_trainers'),
-            'permission_callback' => function () {
-                return current_user_can('read');
-            }
-        ));
-
-        register_rest_route('api/v1', 'users/trainers/(?P<id>[\d]+)', array(
-            'methods' => 'PUT',
-            'callback' => array($this, 'update_trainers'),
-            'permission_callback' => function () {
-                return current_user_can('read');
-            }
-        ));
-
-        register_rest_route('api/v1', 'users/trainers/(?P<id>[\d]+)', array(
-            'methods' => 'DELETE',
-            'callback' => array($this, 'delete_user'),
-            'permission_callback' => function () {
-                return current_user_can('read');
-            }
-        ));
-
-        // Endpoint for creating trainers
-        register_rest_route('api/v1', 'users/trainers', array(
-            'methods' => 'POST',
-            'callback' => array($this, 'create_user'),
-            'permission_callback' => function () {
-                return current_user_can('read');
-            }
-        ));
-
-        // Endpoint for getting trainees
-        register_rest_route('api/v1', 'users/trainees', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_trainees'),
-            'permission_callback' => function () {
-                return current_user_can('read');
-            }
-        ));
-
-        register_rest_route('api/v1', 'users/inactive/trainees', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_inactive_trainees'),
-            'permission_callback' => function () {
-                return current_user_can('manage_options');
-            }
-        ));
-
-        register_rest_route('api/v1', 'users/trainees', array(
-            'methods' => 'POST',
-            'callback' => array($this, 'create_trainee'),
-            'permission_callback' => function () {
-                return current_user_can('edit_posts');
-            }
-        ));
-
-        register_rest_route('api/v1', 'users/trainees/deactivated', array(
-            'methods'             => 'GET',
-            'callback'            => array($this, 'get_deactivated_trainees'),
-            'permission_callback' => function () {
-                return current_user_can('edit_posts');
-            }
-        ));
-        
-
 
 
         // Endpoint for getting project managers (PMs)
@@ -224,29 +155,38 @@ class UsersRoutes
     public function create_user($request)
     {
         $user_data = $request->get_json_params();
-
+    
         // Validate required fields
         if (empty($user_data['fullname']) || empty($user_data['email']) || empty($user_data['role']) || empty($user_data['password']) || empty($user_data['cohort'])) {
             return new WP_Error('create_failed', 'Missing required fields', ['status' => 400]);
         }
-
+    
         $fullname = $user_data['fullname'];
         $email = $user_data['email'];
         $role = $user_data['role'][0]; // Get the first role from the array
         $password = $user_data['password'];
         $cohort = $user_data['cohort']; // Added cohort field
         $is_active = true; // Set the user as active by default
-
+    
         // Validate email address
         if (!is_email($email)) {
             return new WP_Error('create_failed', 'Invalid email address', ['status' => 400]);
         }
-
+    
         // Check if user with the same email already exists
         if (email_exists($email)) {
             return new WP_Error('create_failed', 'User with the same email already exists', ['status' => 409]);
         }
-
+    
+        // Check if the cohort exists
+        global $wpdb;
+        $cohort_table = $wpdb->prefix . 'cohorts';
+        $cohort_exists = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $cohort_table WHERE c_id = %d", $cohort));
+    
+        if (!$cohort_exists) {
+            return new WP_Error('create_failed', 'Cohort does not exist', ['status' => 400]);
+        }
+    
         $user_id = wp_insert_user([
             'user_nicename' => $fullname,
             'user_login' => $email, // Use email as the login name
@@ -254,67 +194,20 @@ class UsersRoutes
             'user_email' => $email,
             'role' => $role,
         ]);
-
+    
         if (is_wp_error($user_id)) {
             return new WP_Error('create_failed', $user_id->get_error_message(), ['status' => 500]);
         }
-
+    
         // Update the user meta for the cohort field
         update_user_meta($user_id, 'cohort', $cohort);
-
+    
         // Update the user meta for the is_active field
         update_user_meta($user_id, 'is_active', $is_active);
-
-        return 'User created successfully';
+    
+        return '200 OK. User created successfully';
     }
-
-
-    public function create_trainee($request)
-    {
-        $user_data = $request->get_json_params();
-
-        // Validate required fields
-        if (empty($user_data['fullname']) || empty($user_data['email']) || empty($user_data['password']) || empty($user_data['cohort'])) {
-            return new WP_Error('create_failed', 'Missing required fields', ['status' => 400]);
-        }
-
-        $fullname = $user_data['fullname'];
-        $email = $user_data['email'];
-        $password = $user_data['password'];
-        $cohort = $user_data['cohort']; // Added cohort field
-        $is_active = true; // Set the user as active by default
-
-        // Validate email address
-        if (!is_email($email)) {
-            return new WP_Error('create_failed', 'Invalid email address', ['status' => 400]);
-        }
-
-        // Check if user with the same email already exists
-        if (email_exists($email)) {
-            return new WP_Error('create_failed', 'User with the same email already exists', ['status' => 409]);
-        }
-
-        $role = 'trainee'; // Set the role as "trainee" for new users
-
-        $user_id = wp_insert_user([
-            'user_nicename' => $fullname,
-            'user_login' => $email, // Use email as the login name
-            'user_pass' => $password,
-            'user_email' => $email,
-            'role' => $role,
-        ]);
-
-        if (is_wp_error($user_id)) {
-            return new WP_Error('create_failed', $user_id->get_error_message(), ['status' => 500]);
-        }
-
-        // Update user meta for cohort and is_active
-        update_user_meta($user_id, 'cohort', $cohort);
-        update_user_meta($user_id, 'is_active', $is_active);
-
-        return 'User created successfully';
-    }
-
+    
 
     public function update_user($request)
     {
@@ -383,123 +276,6 @@ class UsersRoutes
         return $modified_admins;
     }
 
-    public function get_trainers()
-    {
-        $trainers = get_users([
-            'role'         => 'trainer',
-            'meta_key'     => 'is_active',
-            'meta_value'   => true,
-            'meta_compare' => '=',
-        ]);
-    
-        if (empty($trainers)) {
-            return new WP_Error('no_active_trainers_found', 'No active trainers found', ['status' => 404]);
-        }
-    
-        $modified_trainers = array_map(function ($trainer) {
-            return [
-                'ID'       => $trainer->ID,
-                'id'       => $trainer->ID,
-                'fullname' => $trainer->user_nicename,
-                'email'    => $trainer->user_email,
-                'roles'    => $trainer->roles,
-                'cohort'   => (string) get_user_meta($trainer->ID, 'cohort', true),
-            ];
-        }, $trainers);
-    
-        return $modified_trainers;
-    }
-    
-
-
-    public function get_trainees()
-    {
-        $trainees = get_users([
-            'role'         => 'trainee',
-            'meta_key'     => 'is_active',
-            'meta_value'   => true,
-            'meta_compare' => '=',
-        ]);
-
-        if (empty($trainees)) {
-            return new WP_Error('no_trainees_found', 'No trainees found', ['status' => 404]);
-        }
-
-        $modified_trainees = array_map(function ($trainee) {
-            $cohort = get_user_meta($trainee->ID, 'cohort', true);
-
-            return [
-                'ID'       => $trainee->ID,
-                'id'       => $trainee->ID,
-                'fullname' => $trainee->user_nicename,
-                'email'    => $trainee->user_email,
-                'roles'    => $trainee->roles,
-                'cohort'   => $cohort
-            ];
-        }, $trainees);
-
-        return $modified_trainees;
-    }
-
-    public function get_deactivated_trainees()
-{
-    $trainees = get_users([
-        'role'         => 'trainee',
-        'meta_key'     => 'is_active',
-        'meta_value'   => false,
-        'meta_compare' => '=',
-    ]);
-
-    if (empty($trainees)) {
-        return new WP_Error('no_deactivated_trainees_found', 'No deactivated trainees found', ['status' => 404]);
-    }
-
-    $modified_trainees = array_map(function ($trainee) {
-        return [
-            'ID'       => $trainee->ID,
-            'id'       => $trainee->ID,
-            'fullname' => $trainee->user_nicename,
-            'email'    => $trainee->user_email,
-            'roles'    => $trainee->roles,
-            'cohort'   => get_user_meta($trainee->ID, 'cohort', true),
-        ];
-    }, $trainees);
-
-    return $modified_trainees;
-}
-
-
-
-    public function get_inactive_trainees()
-    {
-        $inactive_trainees = get_users([
-            'role'         => 'trainee',
-            'meta_key'     => 'is_active',
-            'meta_value'   => false,
-            'meta_compare' => '=',
-        ]);
-
-        if (empty($inactive_trainees)) {
-            return new WP_Error('no_inactive_trainees_found', 'No inactive trainees found', ['status' => 404]);
-        }
-
-        $modified_trainees = array_map(function ($trainee) {
-            $cohort = get_user_meta($trainee->ID, 'cohort', true);
-
-            return [
-                'ID'       => $trainee->ID,
-                'id'       => $trainee->ID,
-                'fullname' => $trainee->user_nicename,
-                'email'    => $trainee->user_email,
-                'roles'    => $trainee->roles,
-                'cohort'   => $cohort,
-            ];
-        }, $inactive_trainees);
-
-        return $modified_trainees;
-    }
-
-
 
     public function get_pms()
     {
@@ -524,33 +300,6 @@ class UsersRoutes
         return $modified_pms;
     }
 
-    public function update_trainers($request)
-    {
-        $user_data = $request->get_json_params();
-
-        $user_id = $user_data['id'];
-        $fullname = $user_data['fullname'];
-        $email = $user_data['email'];
-        $role = $user_data['role'];
-        $password = $user_data['password'];
-        $cohort = $user_data['cohort']; // Added cohort field
-
-        $user = wp_update_user([
-            'ID' => $user_id,
-            'user_nicename' => $fullname,
-            'user_login' => $email,
-            'user_pass' => $password,
-            'user_email' => $email,
-            'role' => $role,
-            'cohort' => $cohort // Update the cohort value
-        ]);
-
-        if (is_wp_error($user)) {
-            return new WP_Error('update_failed', $user->get_error_message(), ['status' => 404]);
-        }
-
-        return 'User updated successfully';
-    }
 
     public function deactivate_user($request)
     {
