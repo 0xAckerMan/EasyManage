c61aff9f80756eddd307c7b2b3329c997d203051
0xAckerMan <joelkores1@gmail.com>
2023-06-23 18:59:39 +0300

    class create tables foreign key

--- wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
+++ wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
@@ -15,7 +15,7 @@ class CreateTables {
     }
     
 
-    function create_table_projects() {
+        function create_table_projects() {
         global $wpdb;
     
         $table_name = $wpdb->prefix . 'projects';
@@ -29,32 +29,40 @@ class CreateTables {
             p_status INT DEFAULT 0 NOT NULL,
             p_created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             p_due_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            p_assigned_to mediumint(9) NOT NULL,
-            p_assigned_by mediumint(9) NOT NULL,
-            p_cohort_id INT NOT NULL
+            p_assigned_to bigint(20) UNSIGNED NOT NULL,
+            p_assigned_by BIGINT(20) UNSIGNED NOT NULL,
+            p_cohort_id INT NOT NULL,
+            p_group_id INT DEFAULT NULL,
+            FOREIGN KEY (p_assigned_by) REFERENCES {$wpdb->prefix}users(ID),
+            FOREIGN KEY (p_cohort_id) REFERENCES {$wpdb->prefix}cohorts(c_id),
+            FOREIGN KEY (p_group_id) REFERENCES {$wpdb->prefix}groups(group_id)
         )";
     
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($project_details);
     }
+    // FOREIGN KEY (p_assigned_to) REFERENCES {$wpdb->prefix}users(ID),
+
     
 
     function create_table_tasks(){
         global $wpdb;
-
+    
         $table_name = $wpdb->prefix . 'tasks';
-
+    
         $task_details = "CREATE TABLE IF NOT EXISTS " . $table_name . "(
             t_id INT AUTO_INCREMENT PRIMARY KEY,
             t_name VARCHAR(255) NOT NULL,
             t_status INT DEFAULT 0 NOT NULL,
             t_due_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            t_project_id INT NOT NULL
+            t_project_id INT NOT NULL,
+            FOREIGN KEY (t_project_id) REFERENCES {$wpdb->prefix}projects(p_id)
         )";
-
+    
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($task_details);
     }
+    
 
     function create_table_cohorts() {
         global $wpdb;
@@ -74,6 +82,22 @@ class CreateTables {
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($cohort_details);
     }
+
+    function create_table_groups() {
+        global $wpdb;
+    
+        $table_name = $wpdb->prefix . 'groups';
+    
+        $group_details = "CREATE TABLE IF NOT EXISTS " . $table_name . "(
+            group_id INT AUTO_INCREMENT PRIMARY KEY,
+            group_name VARCHAR(255) NOT NULL,
+            group_created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        )";
+    
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($group_details);
+    }
+    
     
     
 }
