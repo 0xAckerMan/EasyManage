5ab0a3aa68664bbeaa45553dd592c0a401874ed7
0xAckerMan <joelkores1@gmail.com>
2023-06-26 21:50:09 +0300

    Updated db schemia

--- wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
+++ wp-content/plugins/EasyManage/inc/Pages/CreateTables.php
@@ -27,18 +27,31 @@ class CreateTables {
             p_status INT DEFAULT 0 NOT NULL,
             p_created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             p_due_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            p_assigned_to bigint(20) UNSIGNED DEFAULT NULL,
             p_assigned_by BIGINT(20) UNSIGNED NOT NULL,
             p_cohort_id INT NOT NULL,
-            p_group_id INT DEFAULT NULL,
             FOREIGN KEY (p_assigned_by) REFERENCES {$wpdb->prefix}users(ID),
-            FOREIGN KEY (p_cohort_id) REFERENCES {$wpdb->prefix}cohorts(c_id),
-            FOREIGN KEY (p_group_id) REFERENCES {$wpdb->prefix}groups_projects(id),
-            FOREIGN KEY (p_assigned_to) REFERENCES {$wpdb->prefix}users(ID)
+            FOREIGN KEY (p_cohort_id) REFERENCES {$wpdb->prefix}cohorts(c_id)
         )";
 
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($project_details);
+    }
+    
+    function create_table_group_project() {
+        global $wpdb;
+    
+        $table_name = $wpdb->prefix . 'group_projects';
+    
+        $group_project_users_details = "CREATE TABLE IF NOT EXISTS " . $table_name . "(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id BIGINT(20) UNSIGNED NOT NULL,
+            project_id INT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES {$wpdb->prefix}users(ID),
+            FOREIGN KEY (project_id) REFERENCES {$wpdb->prefix}projects(p_id)
+        )";
+    
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($group_project_users_details);
     }
 
     function create_table_tasks() {
@@ -79,22 +92,5 @@ class CreateTables {
     }
 
 
-    function create_table_group_project() {
-        global $wpdb;
-    
-        $table_name = $wpdb->prefix . 'group_projects';
-    
-        $group_project_users_details = "CREATE TABLE IF NOT EXISTS " . $table_name . "(
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            group_name VARCHAR(200) NOT NULL,
-            user_id BIGINT(20) UNSIGNED NOT NULL,
-            project_id INT NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES {$wpdb->prefix}users(ID),
-            FOREIGN KEY (project_id) REFERENCES {$wpdb->prefix}projects(p_id)
-        )";
-    
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($group_project_users_details);
-    }
     
 }
