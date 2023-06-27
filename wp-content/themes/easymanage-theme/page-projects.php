be38343d47ec23f724e5556138b97783256cc36e
0xAckerMan <joelkores1@gmail.com>
2023-06-27 21:17:02 +0300

    Works partially for admin

--- wp-content/themes/easymanage-theme/page-projects.php
+++ wp-content/themes/easymanage-theme/page-projects.php
@@ -1,4 +1,43 @@
+<?php if (!is_user_logged_in()) wp_redirect(site_url('/login')) ?>
+
 <?php get_header(); ?>
+
+<?php
+// $trainees = array();
+$users = get_all_users();
+$pms = array_filter($users, function ($user) {
+    return  is_user_in_role($user, 'program-manager');
+});
+$trainers = array_filter($users, function ($trainer) {
+    return  is_user_in_role($trainer, 'trainer');
+});
+$trainees = array_filter($users, function ($trainee) {
+    return is_user_in_role($trainee, 'trainee');
+});
+
+// $projects = [];
+if (is_user_in_role(wp_get_current_user(), 'administrator')) {
+    $projects = get_all_projects();
+} else if (is_user_in_role(wp_get_current_user(), 'ProjectManager')) {
+    $projects = get_trainers_projects(get_current_user_id());
+} else {
+    $projects = get_trainee_projects(get_current_user_id());
+}
+
+// $projects = get_all_projects();
+$ongoing = array_filter($projects, function ($project) {
+    return $project->p_status == 0;
+});
+$completed = array_filter($projects, function ($project) {
+    return $project->p_status == 1;
+});
+?>
+
+<pre>
+    <?php
+    // var_dump($projects);
+    ?>
+</pre>
 
 <div class="project-summary-con">
     <div class="section-header">
@@ -21,34 +60,35 @@
         </div>
 
         <?php
-        $projects = array_fill(0, 1, [
-            'title' => 'Plana - Event Management System',
-            'progress' => '75%',
-            'assigned_to' => 'John Doe, Jane Doe, John Smith',
-            'due_date' => 'Jul 23',
-            'status' => 'Pending',
-            'category' => 'Web App',
-            'tags' => 'WordPress, plugins'
-        ]);
-
-        foreach ($projects as $project) {
-        ?>
-            <div class="project-summary-d">
-                <a href="<?php echo site_url('/detailed-project?id=1') ?>" class="ps-name"><?php echo $project['title'] ?></a>
-                <span class="ps-duedate"><?php echo $project['due_date'] ?></span>
-                <span class="ps-status"><span><?php echo $project['status'] ?></span></span>
-                <span class="ps-assignee"><?php echo $project['assigned_to'] ?></span>
-                <div class="ps-detail">
-                    <span><?php echo $project['category'] ?></span>
-                    <span><?php echo $project['tags'] ?></span>
-                </div>
-                <span class="ps-progress">
-                    <div class="progress">
-                        <div class="progress-bar" style="width: <?php echo $project['progress'] ?>"></div>
-                    </div>
-                </span>
+foreach ($projects as $project) {
+?>
+    <div class="project-summary-d">
+        <a href="<?php echo site_url('/detailed-project?id='.$project->p_id) ?>" class="ps-name"><?php echo $project->p_name ?></a>
+        <span class="ps-duedate"><?php echo style_date($project->p_due_date) ?></span>
+        <span class="ps-status"><span><?php echo $project->p_status == 0 ? "pending" : 'completed' ?></span></span>
+        <span class="ps-assignee">
+            <?php
+            if (isset($project->assigned_users)) {
+                foreach ($project->assigned_users as $assignedUser) {
+                    echo get_fullname_from_users($assignedUser, $trainees) . ', ';
+                }
+                echo rtrim(', ', ', '); // remove the trailing comma and space
+            }
+            ?>
+        </span>
+        <div class="ps-detail">
+            <span><?php echo $project->p_category ?></span>
+            <span><?php echo $project->p_excerpt ?></span>
+        </div>
+        <span class="ps-progress">
+            <div class="progress">
+                <div class="progress-bar" style="width: <?php echo $project->progress ?>"></div>
             </div>
-        <?php } ?>
+        </span>
+    </div>
+<?php
+}
+?>
     </div>
 </div>
 
