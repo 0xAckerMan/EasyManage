80e3946d2bc112bab290b4d6cfed796fbd926611
0xAckerMan <joelkores1@gmail.com>
2023-06-27 21:14:53 +0300

    Handles creation of pms

--- wp-content/themes/easymanage-theme/page-create-program-manager.php
+++ wp-content/themes/easymanage-theme/page-create-program-manager.php
@@ -17,26 +17,30 @@ get_header();
 global $form_error;
 global $form_success;
 
-if (isset($_POST['create-employee-submit'])) {
+if (isset($_POST['create-pm-submit'])) {
 
     $data = [
         'fullname' => $_POST['fullname'],
         'email' => $_POST['email'],
-        'role' => $_POST['role'],
+        // 'role' => 'program-manager',
         'password' => $_POST['password'],
     ];
 
-    $res = wp_remote_post('http://localhost/schedulr/wp-json/api/v1/users/', [
-        'method' => 'POST',
-        'headers' => ['Authorization' => 'Bearer ' . $GLOBALS['token'], 'Content-Type' => 'application/json'],
+    var_dump($data);
+
+    $res = wp_remote_post('http://localhost/EasyManage/wp-json/api/v1/users/program_manager', [
+        'methods' => 'POST',
+        'headers' => ['Content-Type' => 'application/json'],
         'body' => json_encode($data),
         'data_format' => 'body'
     ]);
     $res = wp_remote_retrieve_body($res);
 
+    // var_dump($res);
+
 
     if (!is_wp_error($res)) {
-        $form_success = 'Employee created successfully';
+        $form_success = $res;
     }
 }
 
@@ -46,8 +50,8 @@ if (isset($_POST['create-employee-submit'])) {
 <form action="" method="post">
     <div style="padding: 10px 50px;width:60%; color:dodgerblue" class="span-icon"><ion-icon name='arrow-back'></ion-icon><a href="<?php echo site_url('/users') ?>"> Back to users</a></div>
     <div class="page-create-employee">
-        <div class="inner-form">
-            <h2>Create Program Manager</h2>
+        <div class="inner-form" >
+            <h2 style="font-size: 23px;">Create Program Manager</h2>
 
             <p class="error"><?php echo $form_error; ?></p>
             <p class="success"><?php echo $form_success; ?></p>
@@ -56,18 +60,18 @@ if (isset($_POST['create-employee-submit'])) {
             <?php echo do_shortcode("[input_tag name='email' label='Email Address' input_type='email' placeholder='Enter their email address']") ?>
             <?php echo do_shortcode("[input_tag name='password' label='Password' input_type='password' placeholder='Enter their password']") ?>
 
-            <div class="input-con-radio">
+            <!-- <div class="input-con-radio">
                 <label for="">Role</label>
 
                 <div class="radios">
                     <input type="radio" name="role" id="project-manager" value="ProjectManager" required>
-                    <label for="project-manager">
+                    <label for="program-manager">
                         Program Manager
                     </label>
                 </div>
-            </div>
+            </div> -->
 
-            <button class="custom-btn" type="submit" name="create-employee-submit">Create</button>
+            <button class="custom-btn" type="submit" name="create-pm-submit">Create</button>
         </div>
     </div>
 </form>
