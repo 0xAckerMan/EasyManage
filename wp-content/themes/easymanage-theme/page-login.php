c63fb6b46eba29a800e4f1f92bd4033ba52b9840
0xAckerMan <joelkores1@gmail.com>
2023-06-28 18:04:47 +0300

    Not production ready

--- wp-content/themes/easymanage-theme/page-login.php
+++ wp-content/themes/easymanage-theme/page-login.php
@@ -24,6 +24,23 @@ if (isset($_POST['login-submit'])) {
             'user_password' => $password
         ]);
 
+        $args = [
+            'method'=>'POST',
+            'body'=>[
+                'username'=>$email_username,
+                'password'=>$password
+            ]
+        ];
+
+        $result = wp_remote_post('http://localhost/EasyManage/wp-json/jwt-auth/v1/token', $args);
+
+        $token =(json_decode(wp_remote_retrieve_body($result)));
+        // echo '<pre>';
+        // var_dump(($token));
+            // var_dump($token->token);
+            setcookie('token', $token->token, time() + (86400 * 30), '/', 'localhost');
+        // echo '</pre>';
+
         if (is_wp_error($user)) {
             $form_error =  $user->get_error_message();
         } else {
