6f9db85d539861312745842908c05f9488b17a28
0xAckerMan <joelkores1@gmail.com>
2023-06-28 18:04:11 +0300

    Not production ready

--- wp-content/themes/easymanage-theme/front-page.php
+++ wp-content/themes/easymanage-theme/front-page.php
@@ -4,6 +4,7 @@ if (!is_user_logged_in()) wp_redirect(site_url('/login'));
 
 <?php
 $users = get_all_users();
+// var_dump($users);
 $pms = array_filter($users, function ($user) {
     return  is_user_in_role($user, 'program-manager');
 });
@@ -13,7 +14,6 @@ $trainers = array_filter($users, function ($trainer) {
 $trainees = array_filter($users, function ($trainee) {
     return is_user_in_role($trainee, 'trainee');
 });
-// var_dump($users);
 ?>
 
 
@@ -63,7 +63,7 @@ get_header();
 ?>
 
 <div class="page-home">
-    <?php if (current_user_can('manage_options')) : ?>
+    <?php if (current_user_can('manage_options')) { ?>
         <div class="top-cards-con" style="width: 118%; margin-left: -14%;">
 
             <div class="brief-info-card">
@@ -178,7 +178,7 @@ get_header();
                 foreach ($limitedProjects as $project) {
                 ?>
                     <div class="project-summary-d">
-                    <a href="<?php echo site_url('/detailed-project?id='.$project->p_id) ?>" class="ps-name"><?php echo $project->p_name ?></a>
+                        <a href="<?php echo site_url('/detailed-project?id=' . $project->p_id) ?>" class="ps-name"><?php echo $project->p_name ?></a>
                         <span class="ps-duedate"><?php echo style_date($project->p_due_date) ?></span>
                         <span class="ps-status"><span><?php echo $project->p_status == 0 ? "pending" : 'completed' ?></span></span>
                         <span class="ps-assignee">
@@ -208,9 +208,10 @@ get_header();
 
             </div>
         </div>
-    <?php endif; ?>
+    <?php } ?>
 
     <?php
+    if (is_user_in_role(wp_get_current_user(), 'trainer')) {
         $t_projects = get_trainers_projects(get_current_user_id());
 
         // var_dump($t_projects);
@@ -220,9 +221,10 @@ get_header();
         $t_completed = array_filter($t_projects, function ($t_project) {
             return $t_project->p_status == 1;
         });
+    }
     ?>
 
-    <?php if (current_user_can('trainer'))?>
+    <?php if (current_user_can('trainer')) { ?>
 
         <?php
         // var_dump("$t_active");
@@ -252,7 +254,7 @@ get_header();
 
             <div class="overview-card">
                 <p class="overview-title"> My Projects Overview</p>
-                <p class="overview-total"><?php echo count($t_projects);?></p>
+                <p class="overview-total"><?php echo count($t_projects); ?></p>
                 <div class="overview-percent-con" style="grid-template-columns: 30% 70%;">
                     <div></div>
                     <div></div>
@@ -324,96 +326,109 @@ get_header();
                             </div>
                         </span>
                     </div>
-                <?php
-                }
-
-                ?>
             </div>
-        </div>
-    <?php if (is_user_in_role(wp_get_current_user(), 'trainee')) : ?>
+    <?php
+                }
+            }
+
+    ?>
 
         <?php
-        $s_projects = get_trainee_projects(get_current_user_id());
-        $s_active = get_trainee_active_project(get_current_user_id());
-        $s_completed = array_filter($s_projects, function ($s_project) {
-            return $s_project->p_status == 1;
-        });
-        ?>
-        <div class="overview-flex" style="width: 118%; margin-left: -14%;">
+if (current_user_can('trainee')) {
+?>
 
-            <div class="overview-card">
-                <p class="overview-title">My Projects</p>
-                <p class="overview-total"><?php echo count($s_projects) ?></p>
-                <div class="overview-percent-con" style="grid-template-columns: <?php echo calculate_completion_percentage($s_ongoing, $s_completed) ?>">
-                    <div></div>
-                    <div></div>
+    <?php
+    $s_projects = get_trainee_projects(get_current_user_id());
+    $s_active = get_trainee_active_project(get_current_user_id());
+    $s_completed = get_trainee_completed_project(get_current_user_id());
+    // echo count($s_active);
+    ?>
+
+    <div class="overview-flex" style="width: 118%; margin-left: -14%;">
+
+        <div class="overview-card">
+            <p class="overview-title">My Projects</p>
+            <p class="overview-total"><?php echo count($s_projects) ?></p>
+            <div class="overview-percent-con" style="grid-template-columns: <?php echo calculate_completion_percentage($s_active, $s_completed) ?>">
+                <div></div>
+                <div></div>
+            </div>
+
+            <div class="overview-labels">
+
+                <div>
+                    <div class="ol-title">In progress</div>
+                    <div class="ol-val"><?php echo count($s_active) ?></div>
                 </div>
-
-                <div class="overview-labels">
-
-                    <div>
-                        <div class="ol-title">In progress</div>
-                        <div class="ol-val"><?php echo count($s_active) ?></div>
-                    </div>
-                    <div>
-                        <div class="ol-title">Completed</div>
-                        <div class="ol-val"><?php echo count($s_completed) ?></div>
-                    </div>
+                <div>
+                    <div class="ol-title">Completed</div>
+                    <div class="ol-val"><?php echo count($s_completed) ?></div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="project-summary-con" style="margin-top: 2%; width: 118%; margin-left: -14%;">
-            <div class="section-header">
-                <h3>Projects Summary</h3>
-                <a href="<?php echo site_url('/projects') ?>">View All</a>
+    <div class="project-summary-con" style="margin-top: 2%; width: 118%; margin-left: -14%;">
+        <div class="section-header">
+            <h3>Projects Summary</h3>
+            <a href="<?php echo site_url('/projects') ?>">View All</a>
+        </div>
+
+        <div class="project-summary-list">
+            <div class="project-summary-h">
+                <span class="ps-name">Project Name</span>
+                <span class="ps-duedate">Due Date</span>
+                <span class="ps-status">Status</span>
+                <span class="ps-assignee">Assignee</span>
+                <span class="ps-detail">Project Detail</span>
+                <span class="ps-progress">Progress</span>
             </div>
 
-            <div class="project-summary-list">
-                <div class="project-summary-h">
-                    <span class="ps-name">Project Name</span>
-                    <span class="ps-duedate">Due Date</span>
-                    <span class="ps-status">Status</span>
-                    <span class="ps-assignee">Assignee</span>
-                    <span class="ps-detail">Project Detail</span>
-                    <span class="ps-progress">Progress</span>
+            <?php
+            // echo count(
+            if (count($s_projects) === 0) {
+            ?>
+                <div class="project-task list-border list-empty">
+                    No Projects Found
                 </div>
+            <?php
+            }
+            ?>
 
-                <?php
-                if (count($s_projects) === 0) {
-                ?>
-                    <div class="project-task list-border list-empty">
-                        No Projects Found
-                    </div>
-                <?php
-                }
-                ?>
+<?php
+                $limitedProjects = array_slice($s_active, 0, 1); // Extract the first three projects
 
-                <?php
-
-
-                foreach ($s_projects as $s_project) {
+                foreach ($limitedProjects as $project) {
                 ?>
                     <div class="project-summary-d">
-                        <span class="ps-name"><?php echo $s_project->p_name ?></span>
-                        <span class="ps-duedate"><?php echo style_date($s_project->p_due_date) ?></span>
-                        <span class="ps-status"><span><?php echo $s_project->p_status == 0 ? "pending" : 'completed' ?></span></span>
-                        <span class="ps-assignee"><?php echo get_fullname_from_users($s_project->p_assigned_to, $trainees) ?></span>
+                        <span class="ps-name"><?php echo $project->p_name ?></span>
+                        <span class="ps-duedate"><?php echo style_date($project->p_due_date) ?></span>
+                        <span class="ps-status"><span><?php echo $project->p_status == 0 ? "pending" : 'completed' ?></span></span>
+                        <span class="ps-assignee">
+                            <?php
+                            if (isset($project->assigned_users)) {
+                                foreach ($project->assigned_users as $assignedUser) {
+                                    echo get_fullname_from_users($assignedUser, $trainees) . ', ';
+                                }
+                                echo rtrim(', ', ', '); // remove the trailing comma and space
+                            }
+                            ?>
+                        </span>
                         <div class="ps-detail">
-                            <span><?php echo $s_project->p_category ?></span>
-                            <span><?php echo $s_project->p_excerpt ?></span>
+                            <span><?php echo $project->p_category ?></span>
+                            <span><?php echo $project->p_excerpt ?></span>
                         </div>
                         <span class="ps-progress">
                             <div class="progress">
-                                <div class="progress-bar" style="width: <?php echo $s_project['progress']  ?>"></div>
+                                <div class="progress-bar" style="width: <?php echo $project->progress ?>"></div>
                             </div>
                         </span>
                     </div>
-                <?php } ?>
             </div>
-        </div>
-    <?php endif; ?>
+    <?php
+                }
+            }
+
+    ?>
 </div>
-
-
-<?php get_footer(); ?>
+</div>
